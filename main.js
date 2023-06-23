fc753dab251f4d2f76593721b2d78714e337c0b4
ayichung <121764547+ayichung@users.noreply.github.com>
2023-06-23 15:58:45 -0400

    Add method stubs

---
+++ main.js
@@ -0,0 +1,35 @@
+// features
+// +, -, *, / two nums
+// one op is <num op num>
+
+// set up calc display first, then operate()
+
+// buttons for digits, ops, equals, clear
+// num display as user presses buttons, only 1 num in disp
+// lock highlight on ops buttons when they're pressed but not yet evaled
+
+// bhvr ref https://mrbuddh4.github.io/calculator/
+// bhvr should be like iphone calc, eval nums in pairs as the user strings more
+// ex 1 + 3 - 2 = should turn into 4 - 2 and then 2
+// make sure to handle long decimals
+// handle div by 0
+
+function add() {
+
+}
+
+function sub() {
+
+}
+
+function mult() {
+
+}
+
+function div() {
+
+}
+
+function operate(num1, num2, op) {
+    
+}
